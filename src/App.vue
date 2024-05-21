394c8cba8981351b45e635cda776175bc68088f9
Batug Kocak <batugkocak@gmail.com>
2024-05-21 16:08:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, sunt! Esse
+      animi earum illo aperiam laborum impedit doloribus magnam vitae ipsa
+      eligendi incidunt, commodi praesentium cum provident omnis itaque
+      cupiditate atque autem. Vel animi aliquam qui, distinctio doloremque illum
+      suscipit, laborum maiores iusto odit dolores, facere blanditiis at natus!
+      Sint veritatis cupiditate laboriosam iure cumque magni fugiat, quis
+      nesciunt mollitia voluptatibus illo obcaecati dolorum? Saepe est ad
+      sapiente voluptates? Dignissimos doloremque beatae quos illum totam, nobis
+      ex, nemo dolore labore molestiae eos consequuntur laudantium. Corporis
+      laborum iste at vel hic. Excepturi similique vel nisi assumenda temporibus
+      voluptatibus? Reiciendis, magni fugit.
+    </p>
+    <h2>This is an h2</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit repellat
+      laborum inventore exercitationem? Aspernatur quidem, nisi tempore ad, quos
+      asperiores molestiae, laborum sequi blanditiis nemo pariatur. Quae
+      voluptatibus aperiam magnam consectetur illum ipsum eveniet temporibus?
+      Voluptatem dicta doloremque ratione consectetur dolores? Nulla maxime
+      praesentium optio cum voluptatibus est aliquam dignissimos modi laborum,
+      corrupti aliquid voluptates voluptas sunt, fuga nihil id magnam assumenda
+      iste. Ducimus modi pariatur quibusdam! Quia autem consectetur quisquam
+      eligendi provident nisi expedita maxime possimus aut deserunt, minima
+      necessitatibus amet assumenda tenetur natus optio ab tempore quidem nemo
+      animi! Ipsam earum, quas repudiandae ex officia, magnam repellat delectus
+      tenetur provident illum inventore, harum obcaecati atque odio ipsa aperiam
+      recusandae velit. Iste porro tempora ducimus neque iure corporis quasi
+      repellat dolor quibusdam veniam? Facere iusto quia illum nemo. Mollitia
+      cupiditate corporis cum. Doloribus distinctio dolores, sed hic dolore
+      error praesentium possimus necessitatibus quisquam incidunt adipisci
+      debitis pariatur temporibus impedit, vel laboriosam atque officiis natus
+      similique laborum tempora laudantium eius ratione molestiae! Iste est
+      dolores aut, maxime dolorem repellendus praesentium mollitia ullam cum
+      aliquam rem dolor repudiandae aliquid quaerat excepturi tempora cumque
+      laboriosam magnam. Vitae, odio beatae voluptates officia officiis eius
+      totam ex, rerum molestiae iure repellendus facere repellat. Ab?
+    </p>
+    <h2>This is an h3</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit repellat
+      laborum inventore exercitationem? Aspernatur quidem, nisi tempore ad, quos
+      asperiores molestiae, laborum sequi blanditiis nemo pariatur. Quae
+      voluptatibus aperiam magnam consectetur illum ipsum eveniet temporibus?
+      Voluptatem dicta doloremque ratione consectetur dolores? Nulla maxime
+      praesentium optio cum voluptatibus est aliquam dignissimos modi laborum,
+      corrupti aliquid voluptates voluptas sunt, fuga nihil id magnam assumenda
+      iste. Ducimus modi pariatur quibusdam! Quia autem consectetur quisquam
+      eligendi provident nisi expedita maxime possimus aut deserunt, minima
+      necessitatibus amet assumenda tenetur natus optio ab tempore quidem nemo
+      animi! Ipsam earum, quas repudiandae ex officia, magnam repellat delectus
+      tenetur provident illum inventore, harum obcaecati atque odio ipsa aperiam
+      recusandae velit. Iste porro tempora ducimus neque iure corporis quasi
+      repellat dolor quibusdam veniam? Facere iusto quia illum nemo. Mollitia
+      cupiditate corporis cum. Doloribus distinctio dolores, sed hic dolore
+      error praesentium possimus necessitatibus quisquam incidunt adipisci
+      debitis pariatur temporibus impedit, vel laboriosam atque officiis natus
+      similique laborum tempora laudantium eius ratione molestiae! Iste est
+      dolores aut, maxime dolorem repellendus praesentium mollitia ullam cum
+      aliquam rem dolor repudiandae aliquid quaerat excepturi tempora cumque
+      laboriosam magnam. Vitae, odio beatae voluptates officia officiis eius
+      totam ex, rerum molestiae iure repellendus facere repellat. Ab?
+    </p>
+    <h2>This is an h4</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit repellat
+      laborum inventore exercitationem? Aspernatur quidem, nisi tempore ad, quos
+      asperiores molestiae, laborum sequi blanditiis nemo pariatur. Quae
+      voluptatibus aperiam magnam consectetur illum ipsum eveniet temporibus?
+      Voluptatem dicta doloremque ratione consectetur dolores? Nulla maxime
+      praesentium optio cum voluptatibus est aliquam dignissimos modi laborum,
+      corrupti aliquid voluptates voluptas sunt, fuga nihil id magnam assumenda
+      iste. Ducimus modi pariatur quibusdam! Quia autem consectetur quisquam
+      eligendi provident nisi expedita maxime possimus aut deserunt, minima
+      necessitatibus amet assumenda tenetur natus optio ab tempore quidem nemo
+      animi! Ipsam earum, quas repudiandae ex officia, magnam repellat delectus
+      tenetur provident illum inventore, harum obcaecati atque odio ipsa aperiam
+      recusandae velit. Iste porro tempora ducimus neque iure corporis quasi
+      repellat dolor quibusdam veniam? Facere iusto quia illum nemo. Mollitia
+      cupiditate corporis cum. Doloribus distinctio dolores, sed hic dolore
+      error praesentium possimus necessitatibus quisquam incidunt adipisci
+      debitis pariatur temporibus impedit, vel laboriosam atque officiis natus
+      similique laborum tempora laudantium eius ratione molestiae! Iste est
+      dolores aut, maxime dolorem repellendus praesentium mollitia ullam cum
+      aliquam rem dolor repudiandae aliquid quaerat excepturi tempora cumque
+      laboriosam magnam. Vitae, odio beatae voluptates officia officiis eius
+      totam ex, rerum molestiae iure repellendus facere repellat. Ab?
+    </p>
+  </div>
+</template>
+
+<style></style>
