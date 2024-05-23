e1c34e65356329b01019f74bb717176ee6917fb3
Batug Kocak <batugkocak@gmail.com>
2024-05-23 18:26:17 +0300

    feat: Change HomeView with new Button component TODO: Responsiveness will be added for some components

---
+++ src/components/common/Button.vue
@@ -0,0 +1,68 @@
+<template>
+  <button :class="['custom-button', { special: special }]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "CustomButton",
+  props: {
+    special: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-button {
+  background-color: #333;
+  color: #fff;
+  border: 1px solid #444;
+  padding: 10px 20px;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s, border-color 0.3s;
+  border-radius: 8px;
+  border: 3px solid white;
+}
+
+.custom-button:hover {
+  background-color: #555;
+  border-color: #666;
+}
+
+.custom-button:focus {
+  outline: none;
+  box-shadow: 0 0 5px rgba(255, 255, 255, 0.5);
+}
+
+.special {
+  background-image: linear-gradient(
+    45deg,
+    hsl(290deg 87% 47%) 0%,
+    hsl(355deg 100% 65%) 18%,
+    hsl(53deg 93% 54%) 100%
+  );
+
+  animation: gradientAnimation 3s infinite;
+  background-size: 300% 300%;
+  border: 3px solid white;
+  box-shadow: 0 0 4px white;
+}
+
+@keyframes gradientAnimation {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,87 @@
 <template>
-  <div class="about">
-    <WelcomeText
-      :textList="['Fullstack Developer', 'AI Enthusiast', 'Lifetime Learner']"
-      :writingTime="1500"
-      :delay="4000"
-    />
-    <p>
-      Highly motivated Software Engineering student with a strong foundation in
-      front-end and back-end development. Eager to learn and apply my skills in
-      a professional environment. Possess a strong work ethic and a passion for
-      building user-friendly and secure software solutions.
-    </p>
+  <div class="home">
+    <header>
+      <h1>Batuğ Koçak</h1>
+      <WelcomeText
+        :textList="['Fullstack Developer', 'AI Enthusiast', 'Lifetime Learner']"
+        :writingTime="1500"
+        :delay="4000"
+      />
+      <img
+        src="https://media.licdn.com/dms/image/D4D03AQEmR_u5M7QM9g/profile-displayphoto-shrink_800_800/0/1714295249869?e=1721865600&v=beta&t=X--Bgsx1DuAxKkpcfzOKLJemQP_FysXY_6AGofyEiMg"
+        alt="Batuğ Koçak"
+        class="profile-photo"
+      />
+    </header>
+
+    <section class="about">
+      <p>
+        Highly motivated Software Engineering student with a strong foundation
+        in front-end and back-end development. Eager to learn and apply my
+        skills in a professional environment. Possess a strong work ethic and a
+        passion for building user-friendly and secure software solutions.
+      </p>
+    </section>
+
+    <section class="overview">
+      <div class="overview-item">
+        <Button>Contact Me</Button>
+      </div>
+      <div class="overview-item">
+        <Button :special="true">CV</Button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script setup>
 import WelcomeText from "../components/home/WelcomeText.vue";
+import Button from "../components/common/Button.vue";
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  padding: 20px;
+  max-width: 800px;
+  margin: auto;
+}
+
+header {
+  margin-bottom: 40px;
+}
+
+.profile-photo {
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  margin-top: 20px;
+}
+
+.about {
+  margin-bottom: 40px;
+}
+
+.overview {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin-bottom: 40px;
+}
+
+.overview-item {
+  /* background-color: #222; */
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+}
+
+router-link {
+  color: #ffeb3b;
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
